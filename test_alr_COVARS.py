4136d04f6cf33258218339746d9fd49acecf3fab
Nicolas Ripoll <ripollcab@gmail.com>
2018-02-19 20:22:08 +0100

    covars preproc ready for ALR

--- test_alr_COVARS.py
+++ test_alr_COVARS.py
@@ -3,10 +3,12 @@
 
 import os.path as op
 import xarray as xr
+import pandas as pd
+import numpy as np
 from lib.objs.alr_enveloper import ALR_ENV
 from lib.io.matlab import ReadMatfile as rmat
 from lib.custom_dateutils import DateConverter_Mat2Py
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 # data storage
 p_data = '/Users/ripollcab/Projects/TESLA-kit/teslakit/data/tests_ALR/'
@@ -39,11 +41,18 @@ p_mat = op.join(p_data, 'PCs_for_AWT.mat')
 d_mat = rmat(p_mat)['AWT']
 PCs = d_mat['PCs']
 dates_AWT = [date(r[0],r[1],r[2]) for r in d_mat['Dates']]
+# parse annual data to daily data (using pandas)
+temp = pd.DataFrame(PCs[:,0:3], columns=['PC1','PC2','PC3'], index=dates_AWT)
+PCs_d = temp.reindex(pd.date_range(start=temp.index[0],end=temp.index[-1],freq='D'),method='pad')
+dates_AWT_d = temp.index
+
 
 # make covar data share the same dates
-date_ini = max(dates_MJO[0], dates_AWT[0])
+# TODO: LAS COVAR DEL MJO USAN LA FECHA INICIA AWT. COMENTAR EQUIPO
+date_ini = max(dates_MJO[0], dates_AWT[0], dates_KMA[0])
 date_end = min(dates_MJO[-1], dates_AWT[-1])
-dates_covar = [date_ini + timedelta(days=i) for i in range((date_end-date_ini).days)]
+dates_covar = [date_ini + timedelta(days=i) for i in
+               range((date_end-date_ini).days+1)]
 
 print 'KMA dates:   {0} --- {1}'.format(dates_KMA[0], dates_KMA[-1])
 print ''
@@ -55,9 +64,29 @@ print 'covar dates: {0} --- {1}'.format(dates_covar[0], dates_covar[-1])
 
 # ---------------------------------
 # Mount covariates
-# TODO
 
-import sys; sys.exit()
+# PCs covar
+cov_PCs = PCs_d.loc[dates_covar[0]:dates_covar[-1]]
+cov_1 = cov_PCs.PC1.reshape(-1,1)
+cov_2 = cov_PCs.PC2.reshape(-1,1)
+cov_3 = cov_PCs.PC3.reshape(-1,1)
+
+# MJO covars
+i0 = next(x[0] for x in enumerate(dates_MJO) if x[1] > dates_covar[0])-1
+i1 = next(x[0] for x in enumerate(reversed(dates_MJO)) if x[1] < dates_covar[-1])
+i1 = len(dates_MJO)-i1+1
+cov_4 = np.array(rmm1[i0:i1]).reshape(-1,1)
+cov_5 = np.array(rmm2[i0:i1]).reshape(-1,1)
+
+# join covars
+cov = np.hstack((cov_1, cov_2, cov_3, cov_4, cov_5))
+# TODO totcov, que es? 
+
+
+
+# TODO: SEGUIR AQUI, TENEMOS COVAR, HAY QUE ANADIRLO A LA LIBRERIA ALR
+# EL RESTO DEL CODIGO ES PLACEHOLDER COPIADO DE TESTMJOALR
+
 
 
 # -----------------------------------
