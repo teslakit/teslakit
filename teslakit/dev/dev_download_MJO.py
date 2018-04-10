f2e5f5495373ee0cce4945ae543d15ef07d0df93
Nicolas Ripoll <ripollcab@gmail.com>
2018-04-10 15:50:57 +0200

    data fetcher added to lib for climate data adquisition

--- teslakit/dev/dev_download_MJO.py
+++ teslakit/dev/dev_download_MJO.py
@@ -7,7 +7,7 @@ import sys
 sys.path.insert(0, op.join(op.dirname(__file__),'..'))
 
 # tk libs
-from lib.mjo import DownloadMJO
+from lib.data_fetcher import Download_MJO
 
 # data storage
 p_data = op.join(op.dirname(__file__),'..','data')
@@ -16,6 +16,6 @@ p_mjo_hist = op.join(p_data, 'MJO_hist.nc')
 
 # Download mjo and save xarray.dataset to netcdf
 y1 = '1979-01-01'
-xds_mjo_hist = DownloadMJO(p_mjo_hist, init_year=y1, log=True)
+xds_mjo_hist = Download_MJO(p_mjo_hist, init_year=y1, log=True)
 xds_mjo_hist.to_netcdf(p_mjo_hist)
 
---
+++ teslakit/lib/data_fetcher.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import xarray as xr
+from datetime import datetime
+
+
+def Download_MJO(p_ncfile, init_year=None, log=False):
+    '''
+    Download MJO data and stores it on netcdf format
+    init_year: optional, data before init_year will be discarded ('yyyy-mm-dd')
+    '''
+
+    # default parameter
+    url_mjo = 'http://www.bom.gov.au/climate/mjo/graphics/rmm.74toRealtime.txt'
+
+    # download data and mount time array
+    ddata = np.genfromtxt(
+        url_mjo,
+        skip_header=2,
+        usecols=(0,1,2,3,4,5,6),
+        dtype = None,
+        names = ('year','month','day','RMM1','RMM2','phase','amplitude'),
+    )
+
+    # mount dattime array
+    dtimes = [datetime(d['year'], d['month'], d['day']) for d in ddata]
+
+    # parse data to xarray.Dataset
+    ds_mjo = xr.Dataset(
+        {
+            'mjo'   :(('time',), ddata['amplitude']),
+            'phase' :(('time',), ddata['phase']),
+            'rmm1'  :(('time',), ddata['RMM1']),
+            'rmm2'  :(('time',), ddata['RMM2']),
+        },
+        {'time' : dtimes}
+    )
+
+    # cut dataset if asked
+    if init_year:
+        ds_mjo = ds_mjo.loc[dict(time=slice(init_year, None))]
+
+    # save at netcdf file
+    ds_mjo.to_netcdf(p_ncfile,'w')
+
+    if log:
+        print '\nMJO historical data downloaded to \n{0}\nMJO time: {1} - {2}\n'.format(
+            p_ncfile, ds_mjo.time.values[0],ds_mjo.time.values[-1])
+
+    return ds_mjo
+
