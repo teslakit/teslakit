7ec74aca98f545aa336f233c8a2b26d833718207
Nicolas Ripoll <ripollcab@gmail.com>
2018-05-10 13:48:46 +0200

    test estela PCA from matdata fixed

--- teslakit/tests/test_estela_pca_matdata.py
+++ teslakit/tests/test_estela_pca_matdata.py
@@ -6,9 +6,11 @@ import os.path as op
 import sys
 sys.path.insert(0, op.join(op.dirname(__file__),'..'))
 
+# python libs
 import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
+from sklearn.decomposition import PCA
 
 # tk libs
 from lib.io.matlab import ReadMatfile
@@ -18,36 +20,27 @@ from lib.predictor import CalcPCA_EstelaPred as CalcPCA
 p_data = op.join(op.dirname(__file__),'..','data')
 p_test = op.join(p_data, 'tests_estela_PCA')
 
+p_PCA = op.join(p_test, 'xds_SLP_PCA.nc')
+
 # use same data as matlab test to compare
-p_mat_data = op.join(p_test, 'splgrd1d.mat')
+p_mat_data = op.join(p_test, 'SlpGrd.mat')
 
 
 # --------------------------------------
 # load test data 
 dmat = ReadMatfile(p_mat_data)
 slp_grd_1d = dmat['SlpGrd']
-
 # this raw data is already in [ntimeXnpoints] shape
+
 
 # --------------------------------------
 # PCA
 
 # standarize predictor
-print slp_grd_1d.shape
 slp_grd_mean = np.mean(slp_grd_1d, axis=0)
 slp_grd_std = np.std(slp_grd_1d, axis=0)
 slp_grd_norm = (slp_grd_1d[:,:] - slp_grd_mean) / slp_grd_std
 slp_grd_norm[np.isnan(slp_grd_norm)] = 0
-
-# TODO: PLOTEO PARA COMPARAR DATOS MATLAB CON TKIT
-xtest = xr.Dataset(
-    {'test':(('time','points'),slp_grd_norm)}
-)
-#xtest.test.plot(vmin=-1,vmax=1)
-#plt.show()
-
-
-
 
 
 
@@ -56,7 +49,6 @@ slp_grd_norm_cal = slp_grd_norm
 slp_grd_norm_val = np.array([])
 
 # principal components analysis
-from sklearn.decomposition import PCA
 ipca = PCA(n_components=slp_grd_norm_cal.shape[0])
 PCs = ipca.fit_transform(slp_grd_norm_cal)
 
@@ -68,13 +60,8 @@ xds_PCA = xr.Dataset(
         'EOFs': (('n_components','n_features'), ipca.components_),
         'variance': (('n_components',), ipca.explained_variance_),
     },
-
-    attrs = {
-    }
 )
 
-print xtest
-print xds_PCA
-
-# TODO STORE SAME DATA AS MATLAB CODE
+# store PCA data for tests
+xds_PCA.to_netcdf(p_PCA)
 
