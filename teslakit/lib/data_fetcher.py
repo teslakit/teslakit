47db0c52e5c2e0a9b6175992a20d33387eb3d9fa
Nicolas Ripoll <ripollcab@gmail.com>
2018-05-17 16:09:18 +0200

    dev CSIRO spec downloader

--- teslakit/lib/data_fetcher.py
+++ teslakit/lib/data_fetcher.py
@@ -54,19 +54,12 @@ def Download_MJO(p_ncfile, init_year=None, log=False):
 
     return ds_mjo
 
-def Download_CSIRO(p_ncfile, lonq, latq, var_names):
+def Generate_CSIRO_urls(switch_db='gridded', grid_code='pac_4m'):
     '''
-    Download CSIRO data and stores it on netcdf format
-    lonq, latq: longitude latitude query: single value or limits
+    Generate URL list for downloading csiro gridded/spec data
+    switch_db = gridded / spec
+    grid_code = 'pac_4m', 'aus_4m', 'aus_10m', 'glob_24m', 'pac_4m', 'pac_10m'
     '''
-
-    code = 'pac_4m'  # 'aus_4m', 'aus_10m', 'glob_24m', 'pac_4m', 'pac_10m'
-
-    # long, lat query
-    lonp1 = lonq[0]
-    latp1 = latq[0]
-    lonp2 = lonq[-1]
-    latp2 = latq[-1]
 
     # parameters
     url_base = 'http://data-cbr.csiro.au/thredds/dodsC/catch_all/CMAR_CAWCR-Wave_archive/'
@@ -75,25 +68,53 @@ def Download_CSIRO(p_ncfile, lonq, latq, var_names):
     url_3 = 'CAWCR_Wave_Hindcast_Jun_2013_-_Jul_2014/'
 
     # generate .nc url list
-    l_ym = [
-        '{0}{1:02d}'.format(x, y) for x in range(1979,2010+1) for y in range(1,13)]
-    l_urls_1 = [
-        '{0}{1}gridded/ww3.{2}.{3}.nc'.format(url_base, url_1, code, ym) for ym in l_ym]
+    ym1 = ['{0}{1:02d}'.format(x, y) for x in range(1979,2010+1) for y in range(1,13)]
+    ym2 = ['{0}{1:02d}'.format(x, y) for x in range(2011,2013+1) for y in range(1,13)]
+    ym3 = ['{0}{1:02d}'.format(x, y) for x in range(2013,2014+1) for y in range(1,13)]
 
-    l_ym = [
-        '{0}{1:02d}'.format(x, y) for x in range(2011,2013+1) for y in range(1,13)]
-    l_ym = l_ym[:l_ym.index('201305')+1]
-    l_urls_2 = [
-        '{0}{1}gridded/ww3.{2}.{3}.nc'.format(url_base, url_2, code, ym) for ym in l_ym]
+    ym2 = ym2[:ym2.index('201305')+1]
+    ym3 = ym3[ym3.index('201306'):ym3.index('201407')+1]
 
-    l_ym = [
-        '{0}{1:02d}'.format(x, y) for x in range(2013,2014+1) for y in range(1,13)]
-    l_ym = l_ym[l_ym.index('201306'):l_ym.index('201407')+1]
-    l_urls_3 = [
-        '{0}{1}gridded/ww3.{2}.{3}.nc'.format(url_base, url_3, code, ym) for ym in l_ym]
+    if switch_db == 'gridded':
+        l_urls_1 = ['{0}{1}gridded/ww3.{2}.{3}.nc'.format(
+            url_base, url_1, grid_code, ym) for ym in ym1]
+
+        l_urls_2 = ['{0}{1}gridded/ww3.{2}.{3}.nc'.format(
+            url_base, url_2, grid_code, ym) for ym in ym2]
+
+        l_urls_3 = ['{0}{1}gridded/ww3.{2}.{3}.nc'.format(
+            url_base, url_3, grid_code, ym) for ym in ym3]
+
+    elif switch_db == 'spec':
+        l_urls_1 = ['{0}{1}spec/ww3.{2}_spec.nc'.format(
+            url_base, url_1, ym) for ym in ym1]
+
+        l_urls_2 = ['{0}{1}spec/ww3.{2}_spec.nc'.format(
+            url_base, url_2, ym) for ym in ym2]
+
+        l_urls_3 = ['{0}{1}spec/ww3.{2}_spec.nc'.format(
+            url_base, url_3, ym) for ym in ym3]
 
     l_urls = l_urls_1 + l_urls_2 + l_urls_3
+    return l_urls
 
+def Download_CSIRO_Grid(p_ncfile, lonq, latq, var_names):
+    '''
+    Download CSIRO gridded data and stores it on netcdf format
+    lonq, latq: longitude latitude query: single value or limits
+    var_names: variables to extract
+    '''
+
+    grid_code = 'pac_4m'  # 'aus_4m', 'aus_10m', 'glob_24m', 'pac_4m', 'pac_10m'
+
+    # long, lat query
+    lonp1 = lonq[0]
+    latp1 = latq[0]
+    lonp2 = lonq[-1]
+    latp2 = latq[-1]
+
+    # Generate URL list 
+    l_urls = Generate_CSIRO_urls('gridded', grid_code)
 
     # get coordinates from first file
     with xr.open_dataset(l_urls[0]) as ff:
@@ -112,7 +133,7 @@ def Download_CSIRO(p_ncfile, lonq, latq, var_names):
     with xr.open_dataset(l_urls[-1]) as lf:
         t2 = lf.time[-1].values  # time end
 
-    # get time array
+    # mount time array
     base_time = np.arange(t1, t2, timedelta(hours=1))
 
     # get lon, lat slice
@@ -143,9 +164,9 @@ def Download_CSIRO(p_ncfile, lonq, latq, var_names):
 
 
     # download data from files
+    print 'downloading CSIRO gridded data...'
     for u in l_urls:
         print op.basename(u)
-        #tts = time.time()
 
         # read file from url
         with xr.open_dataset(u) as xds_u:
@@ -161,10 +182,115 @@ def Download_CSIRO(p_ncfile, lonq, latq, var_names):
             for vn in var_names:
                 xds_out[vn][ti0:ti1+1,:,:] = cut[vn].values[:]
 
-        #print time.time()-tts
+    print 'done.'
 
     # save to netcdf file
     xds_out.to_netcdf(p_ncfile, 'w')
 
     return xr.open_dataset(p_ncfile)
 
+def Download_CSIRO_Spec(p_ncfile, lon_p, lat_p):
+    '''
+    Download CSIRO spec data and stores it on netcdf format
+    lon_p, lat_p: longitude latitude point query
+    var_names: variables to extract
+    '''
+
+    # Generate URL list 
+    l_urls = Generate_CSIRO_urls('spec')
+
+    # TODO: QUITAR
+    l_urls = l_urls[:2]
+
+    # get nearest station ID from first file
+    with xr.open_dataset(l_urls[0]) as ff:
+        slons = ff.longitude.values[0,:]
+        slats = ff.latitude.values[0,:]
+
+        # TODO: DIST DIFERENTE PARA LON LATITUDE
+        station_ix = (
+            np.sqrt(np.square(slats-lat_p)+np.square(slons-lon_p))
+        ).argmin()
+
+        lon_station = slons[station_ix]
+        lat_station = slats[station_ix]
+
+        print 'station ix: {0}. Longitude: {1}, Latitude: {2}'.format(
+            station_ix, lon_station, lat_station
+        )
+
+        t1 = ff.time[0].values  # time ini
+
+        # store frequench and direction
+        frequency = ff.frequency.values
+        direction = ff.direction.values
+
+        # store var attrs
+        d_vatrs = {}
+        d_vatrs['Efth'] = ff['Efth'].attrs
+
+    # get time end from last file
+    with xr.open_dataset(l_urls[-1]) as lf:
+        t2 = lf.time[-1].values  # time end
+
+    # mount time array
+    base_time = np.arange(t1, t2, timedelta(hours=1))
+
+    # generate output holder 
+    xds_out = xr.Dataset({},
+        coords = {
+            'time': base_time,
+            'longitude': lon_station,
+            'latitude': lat_station,
+            'frequency': frequency,
+            'direction': direction,
+        }
+    )
+
+    # add vars to output holder
+    xds_out['Efth'] = (
+        ('time', 'frequency', 'direction'),
+        np.nan * np.ones((
+            len(base_time),
+            len(frequency),
+            len(direction)
+        )),
+       d_vatrs['Efth']
+    )
+
+    # download data from files
+    print 'downloading CSIRO spec data...'
+    for u in l_urls:
+        print op.basename(u)
+
+        # read file from url
+        with xr.open_dataset(u) as xds_u:
+            cut_time = np.array(xds_u.time.values, dtype='datetime64[h]')
+            ti0 = np.where(base_time==cut_time[0])[0][0]
+            ti1 = np.where(base_time==cut_time[-1])[0][0]
+
+
+            # TODO TESTS
+            cut = xds_u.isel(station=station_ix)
+            print cut
+
+            # TODO: PARECE QUE NO PODEMOS CARGAR ESTE DATO
+            Efth = cut['Efth'].values[:]
+            print ''
+            print Efth.shape
+
+
+            # fill output vars
+            xds_out['Efth'][ti0:ti1+1,:,:] = cut['Efth'].values[:]
+
+            print 'done'
+            print xds_out
+            import sys; sys.exit()
+
+
+    print 'done.'
+
+    # save to netcdf file
+    xds_out.to_netcdf(p_ncfile, 'w')
+
+    return xr.open_dataset(p_ncfile)
--- teslakit/tests/test_download_CSIRO.py
+++ teslakit/tests/test_download_CSIRO.py
@@ -10,31 +10,37 @@ import numpy as np
 import xarray as xr
 
 # tk libs
-from lib.data_fetcher import Download_CSIRO
+from lib.data_fetcher import Download_CSIRO_Grid, Download_CSIRO_Spec
 
 # data storage
 p_data = op.join(op.dirname(__file__),'..','data')
 
 p_test = op.join(p_data, 'test_CSIRO')
 p_nc_point = op.join(p_test, 'csiro_down_point.nc')
+p_nc_spec = op.join(p_test, 'csiro_down_spec.nc')
 p_nc_mesh = op.join(p_test, 'csiro_down_mesh.nc')
 
 
 # --------------------------------------
-# parameters 
-var_names = ['hs2', 'tp0']
+
+## download point gridded data
+#lonp = [171]
+#latp = [7.5]
+#var_names_grid = ['hs2', 'tp0']
+#xds_p_grid = Download_CSIRO_Grid(p_nc_point, lonp, latp, var_names_grid)
+#print xds_p_grid
 
 
-# download point data
+## download point spec data
 lonp = [171]
 latp = [7.5]
-#xds_p = Download_CSIRO(p_nc_point, lonp, latp, var_names)
-#print xds_p
+xds_p_spec = Download_CSIRO_Spec(p_nc_spec, lonp, latp)
+print xds_p_spec
 
 
-# download mesh data 
-lonm = [170, 173]
-latm = [7, 8]
-xds_m = Download_CSIRO(p_nc_mesh, lonm, latm, var_names)
-print xds_m
+## download mesh data 
+#lonm = [170, 173]
+#latm = [7, 8]
+#xds_m = Download_CSIRO(p_nc_mesh, lonm, latm, var_names)
+#print xds_m
 
