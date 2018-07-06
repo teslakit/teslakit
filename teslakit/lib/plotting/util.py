a67eaff436493ccf882b52545bf7c573ec65e234
Nicolas Ripoll <ripollcab@gmail.com>
2018-07-06 09:48:07 +0200

    added custom colorbar with not centered midvalue

---
+++ teslakit/lib/plotting/util.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.colors as colors
+
+
+class MidpointNormalize(colors.Normalize):
+	'''
+	Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+	e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+	'''
+
+	def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+		self.midpoint = midpoint
+		colors.Normalize.__init__(self, vmin, vmax, clip)
+
+	def __call__(self, value, clip=None):
+		x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+		return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
+
