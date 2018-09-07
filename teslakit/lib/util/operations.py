3e3f0959876c1382c538b275b69038109bd117fb
Nicolas Ripoll <ripollcab@gmail.com>
2018-09-07 13:21:59 +0200

    GetUniqueRows for numpy 2D array function added

--- teslakit/lib/util/operations.py
+++ teslakit/lib/util/operations.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import collections
+import numpy as np
+
+
 def GetDivisors(x):
     l_div = []
     i = 1
@@ -9,3 +13,20 @@ def GetDivisors(x):
             l_div.append(i)
         i = i + 1
     return l_div
+
+def GetUniqueRows(np_array):
+    d = collections.OrderedDict()
+    for a in np_array:
+        t = tuple(a)
+        if t in d:
+            d[t] += 1
+        else:
+            d[t] = 1
+
+    result = []
+    for (key, value) in d.items():
+        result.append(list(key) + [value])
+
+    np_result = np.asarray(result)
+    return np_result
+
