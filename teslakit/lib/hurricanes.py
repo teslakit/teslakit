d1d94100bf309c2e49ead19909c2f77faf0930e2
Nicolas Ripoll <ripollcab@gmail.com>
2018-09-07 13:19:49 +0200

    Functions to get storm category and sort category-change-count matrix added

--- teslakit/lib/hurricanes.py
+++ teslakit/lib/hurricanes.py
@@ -25,6 +25,7 @@ def Extract_Circle(xds_hurr, p_lon, p_lat, r):
     # get storms inside circle area
     n_storms = xds_hurr.storm.shape[0]
     l_storms_area = []
+    l_pos_in = []
 
     for i_storm in range(n_storms):
         lonlat_s = np.column_stack(
@@ -36,9 +37,11 @@ def Extract_Circle(xds_hurr, p_lon, p_lat, r):
         pos_in = np.where(dist<r)[0][:]
         if pos_in.any():
             l_storms_area.append(i_storm)
+            l_pos_in.append(np.array(pos_in))
 
     # cut storm dataset to selection
     xds_area = xds_hurr.isel(storm=l_storms_area)
+    xds_area['pos_inside'] =(('storm',), np.array(l_pos_in))
     return xds_area
 
 def Extract_Square(xds_wmo):
@@ -66,3 +69,48 @@ def Extract_Polygon(xds_wmo):
     # TODO
 
     return None
+
+def GetStormCategory(pres_min):
+    '''
+    Returns storm category (int 5-0)
+    '''
+
+    pres_lims = [920, 944, 964, 979, 1000]
+
+    if pres_min < pres_lims[0]:
+        return 5
+    elif pres_min < pres_lims[1]:
+        return 4
+    elif pres_min < pres_lims[2]:
+        return 3
+    elif pres_min < pres_lims[3]:
+        return 2
+    elif pres_min < pres_lims[4]:
+        return 1
+    else:
+        return 0
+
+def SortCategoryCount(np_categ, nocat=9):
+    '''
+    Sort category change - count matrix
+    np_categ = [[category1, category2, count], ...]
+    '''
+
+
+    categs = [0,1,2,3,4,5,9]
+
+    np_categ = np_categ.astype(int)
+    np_sort = np.empty((len(categs)*(len(categs)-1),3))
+    rc=0
+    for c1 in categs[:-1]:
+        for c2 in categs:
+            p_row = np.where((np_categ[:,0]==c1) & (np_categ[:,1]==c2))
+            if p_row[0].size:
+                np_sort[rc,:]=[c1,c2,np_categ[p_row,2]]
+            else:
+                np_sort[rc,:]=[c1,c2,0]
+
+            rc+=1
+
+    return np_sort.astype(int)
+
