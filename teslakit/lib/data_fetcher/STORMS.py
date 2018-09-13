a5653cb4a85ffbd45c27bed9ef312dd27b5e645a
Nicolas Ripoll <ripollcab@gmail.com>
2018-09-13 11:44:36 +0200

    renamed data_fetcher library

---
+++ teslakit/lib/data_fetcher/STORMS.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import os.path as op
+import urllib
+import xarray as xr
+
+
+def Download_NOAA_WMO(p_ncfile):
+    '''
+    Download storms (Allstorms) netcdf from NOAA
+
+    ftp file name: Allstorms.ibtracs_all.v03rXX.nc.
+    ftp wind velocity in knots: x1.82 km/h
+
+    returns xarray.Dataset
+    xds_HURRs:
+
+        # TODO
+        (time, ) VAR1
+        (time, ) WVAR2
+    '''
+
+    # default parameters
+    ftp_down = 'ftp://eclipse.ncdc.noaa.gov/pub/ibtracs/v03r10/wmo/netcdf/'
+    fil_down = 'Allstorms.ibtracs_wmo.v03r10.nc.gz'
+
+    p_down = op.dirname(p_ncfile)
+    p_gz = op.join(p_down, '{0}'.format(fil_down))
+
+    # download gz
+    ftp_wmo = '{0}{1}'.format(ftp_down, fil_down)
+    urllib.urlretrieve(ftp_wmo, p_gz)
+
+    # TODO descomprimir gz
+
+    # TODO: CONTINUAR CUANDO EL FTP VUELVA
+    # TODO: no funciona mi ftp??
+
