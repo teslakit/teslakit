8b7f5fb2b26653e205fdd4e7650b1083260984e9
Nicolas Ripoll <ripollcab@gmail.com>
2018-09-13 11:51:36 +0200

    pathcontrol added

--- teslakit/dev/dev_WAVES_Families.py
+++ teslakit/dev/dev_WAVES_Families.py
@@ -11,23 +11,25 @@ import numpy as np
 import xarray as xr
 
 # tk libs
+from lib.objs.tkpaths import PathControl
 from lib.io.matlab import ReadGowMat
 from lib.waves import GetDistribution
 
-# data storage
-p_data = op.join(op.dirname(__file__), '..', 'data')
-p_waves = op.join(p_data, 'WAVES')
 
-# input data
-p_waves_parts_p1 = op.join(p_waves, 'partitions', 'point1.mat' )
+# data storage and path control
+p_data = op.join(op.dirname(__file__), '..', 'data')
+pc = PathControl(p_data)
 
 
 # Load wave partitions
-xds_waves_parts = ReadGowMat(p_waves_parts_p1)
+xds_waves_parts = ReadGowMat(pc.p_st_waves_part_p1)
 
 # calculate families
 sectors = [(210, 22.5), (22.5, 135)]
 xds_waves_fam = GetDistribution(xds_waves_parts, sectors)
+
 print xds_waves_fam
-print ''
+
+
+# TODO: CARGAR LOS STORMS Y SEPARAR DATOS DE PARTICIONES
 
