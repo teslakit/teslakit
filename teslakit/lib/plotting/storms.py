920d6d225a3f487a3bcc78f9f0e8b412ea3f8d89
Nicolas Ripoll <ripollcab@gmail.com>
2018-09-13 10:59:09 +0200

    added empty library for storms track plotting

---
+++ teslakit/lib/plotting/storms.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import os.path as op
+import numpy as np
+import matplotlib.pyplot as plt
+
+# TODO
+def WorldMap_Storms(xds_storms):
+    return None
+
+
+# TODO: add otra grafica para mostrar numero de tcs para cada categoria
