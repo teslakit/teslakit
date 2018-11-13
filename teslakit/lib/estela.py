2d1321c3a6780ff12379195b36ad18f630fb0646
Nicolas Ripoll <ripollcab@gmail.com>
2018-11-13 18:13:10 +0100

    estela gradient calculation greatly optimized

--- teslakit/lib/estela.py
+++ teslakit/lib/estela.py
@@ -30,15 +30,32 @@ def spatial_gradient(xdset, var_name):
     for it in range(len(xdset.time)):
         var_val = xdset[var_name].isel(time=it).values
 
-        for i in range(1, Mx-1):
-            for j in range(1, My-1):
-                phi = np.pi*np.abs(lat[j])/180.0
-                dpx1 = (var_val[j,i]   - var_val[j,i-1]) / np.cos(phi)
-                dpx2 = (var_val[j,i+1] - var_val[j,i])   / np.cos(phi)
-                dpy1 = (var_val[j,i]   - var_val[j-1,i])
-                dpy2 = (var_val[j+1,i] - var_val[j,i])
+        # calculate gradient (matrix)
+        m_c = var_val[1:-1,1:-1]
+        m_l = np.roll(var_val, -1, axis=1)[1:-1,1:-1]
+        m_r = np.roll(var_val, +1, axis=1)[1:-1,1:-1]
+        m_u = np.roll(var_val, -1, axis=0)[1:-1,1:-1]
+        m_d = np.roll(var_val, +1, axis=0)[1:-1,1:-1]
+        m_phi = np.pi*np.abs(lat)/180.0
+        m_phi = m_phi[1:-1]
 
-                var_grad[it, j, i] = (dpx1**2+dpx2**2)/2 + (dpy1**2+dpy2**2)/2
+        dpx1 = (m_c - m_l)/np.cos(m_phi[:,None])
+        dpx2 = (m_r - m_c)/np.cos(m_phi[:,None])
+        dpy1 = m_c - m_d
+        dpy2 = m_u - m_c
+
+        vg = (dpx1**2+dpx2**2)/2 + (dpy1**2+dpy2**2)/2
+        var_grad[it, 1:-1, 1:-1] = vg
+
+        # calculate gradient (for). old code
+        #for i in range(1, Mx-1):
+        #    for j in range(1, My-1):
+        #        phi = np.pi*np.abs(lat[j])/180.0
+        #        dpx1 = (var_val[j,i]   - var_val[j,i-1]) / np.cos(phi)
+        #        dpx2 = (var_val[j,i+1] - var_val[j,i])   / np.cos(phi)
+        #        dpy1 = (var_val[j,i]   - var_val[j-1,i])
+        #        dpy2 = (var_val[j+1,i] - var_val[j,i])
+        #        var_grad[it, j, i] = (dpx1**2+dpx2**2)/2 + (dpy1**2+dpy2**2)/2
 
     # store gradient
     xdset['{0}_gradient'.format(var_name)]= (
