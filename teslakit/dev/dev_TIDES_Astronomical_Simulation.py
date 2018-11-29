8eee90a9a4a59d8ced6c031eafb35e1b0eae61db
Nicolas Ripoll <ripollcab@gmail.com>
2018-11-29 16:05:03 +0100

    astronomical tide simulation. some plots added

--- teslakit/dev/dev_TIDES_Astronomical_Simulation.py
+++ teslakit/dev/dev_TIDES_Astronomical_Simulation.py
@@ -10,8 +10,7 @@ sys.path.insert(0, op.join(op.dirname(__file__),'..'))
 # python libs
 import numpy as np
 import xarray as xr
-import netCDF4
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # custom libs
 from ttide.t_tide import t_tide
@@ -19,8 +18,8 @@ from ttide.t_predic import t_predic
 
 # tk libs
 from lib.objs.tkpaths import Site
-from lib.io.matlab import ReadAstroTideMat
-from lib.io.aux_nc import StoreAstroTide
+from lib.io.aux_nc import StoreBugXdset as sbxds
+from lib.plotting.tides import Plot_AstronomicalTide, Plot_ValidateTTIDE
 
 # TODO: ACTUALIZAR A LIBRERIA UTIDE
 # https://www.eoas.ubc.ca/~rich/#T_Tide
@@ -32,11 +31,13 @@ site = Site('KWAJALEIN')
 site.Summary()
 
 # input files
-# TODO: REVISAR/MODIFICAR DATOS TIDE Y USAR iNPUT .NC
-p_tide_astro = site.pc.site.tds.MAR_1820000
+p_astro_fit = site.pc.site.tds.hist_astro
 
 # output files
 p_astro_sim = site.pc.site.tds.sim_astro
+
+# export figs
+p_export_tds = site.pc.site.exp.tds
 
 # Simulation dates
 d1_sim = np.datetime64(site.params.SIMULATION.date_ini)
@@ -45,7 +46,7 @@ d2_sim = np.datetime64(site.params.SIMULATION.date_end)
 
 # --------------------------------------
 # load astronomical tide data
-xds_atide = ReadAstroTideMat(p_tide_astro)
+xds_atide = xr.open_dataset(p_astro_fit)
 xds_atide.rename(
     {'observed':'level',
      'predicted':'tide',
@@ -54,22 +55,46 @@ xds_atide.rename(
 # remove tide nanmin
 xds_atide['tide'] = xds_atide.tide - np.nanmin(xds_atide.tide)
 
-# TODO: TTIDE LIBRARY NO PREPARADA PARA MANEJAR MAS DE 18.6 YEARS
+# data length has to be lesser than 18.6 years
 dt_cut = np.datetime64('1998-06-01')
-xds_atide = xds_atide.where(xds_atide.time>=dt_cut,drop=True)
+xds_atide = xds_atide.where(xds_atide.time >= dt_cut, drop=True)
+
+# Plot astronomical tide
+time = xds_atide.time.values[:]
+tide = xds_atide.tide.values[:]
+p_export = op.join(p_export_tds, 'astronomical_tide.png')
+Plot_AstronomicalTide(time, tide, p_export)
 
 
 # --------------------------------------
 # t_tide library - Fit
 
+# TODO: ttide ha de se calibrada con un year de datos (no 18.6)
 lat0 = 9.75
 d_out = t_tide(xds_atide.tide.values, dt=1, lat=np.array(lat0))
-# TODO: ttide ha de se calibrada con un year de datos (no 18.6)
 
 # variables used for prediction
 names = d_out['nameu']
 freq = d_out['fu']
 tidecon = d_out['tidecon']
+
+
+# --------------------------------------
+# t_tide library - Validation
+
+d1_val = xds_atide.time.values[0]
+d2_val = xds_atide.time.values[-1]
+date_val = np.arange(d1_val, d2_val, dtype='datetime64[h]')
+tide_tt = t_predic(
+    date_val, names, freq, tidecon,
+    lat=lat0, ltype='nodal')
+
+# Plot ttide validation 
+time = xds_atide.time.values[:-1]
+tide = xds_atide.tide.values[:-1]
+tide = tide - np.nanmean(tide)
+p_export = op.join(p_export_tds, 'ttide_validation.png')
+Plot_ValidateTTIDE(time, tide, tide_tt, p_export)
 
 
 # --------------------------------------
@@ -80,40 +105,22 @@ atide_pred = t_predic(
     lat=lat0, ltype='nodal')
 
 
-# --------------------------------------
-# use netCDF4 library to store this data
-StoreAstroTide(p_astro_sim, date_pred, atide_pred)
+# Store data
+dt_pred = [d.astype(datetime) for d in date_pred]
+xds_atide_sim = xr.Dataset(
+    {
+        'tide'   :(('time',), atide_pred),
+    },
+    {'time' : dt_pred}
+)
+
+# xarray.Dataset.to_netcdf() wont work with this time array and time dtype
+sbxds(xds_atide_sim, p_astro_sim)
 print('\nAstronomical Tide Simulation stored at:\n{0}\n'.format(p_astro_sim))
-# TODO: GENERALIZAR A SBXDS??
 
-
-# --------------------------------------
-# TODO: t_tide library validation
-validate_ttide = False
-if validate_ttide:
-
-    # astronomical tide prediction
-    # TODO: COGER ESTAS FECHAS AUTO, ES PARA COMPARAR
-    dp1 = np.datetime64('1998-06-01')
-    dp2 = np.datetime64('2016-12-31')
-    date_pred = np.arange(dp1, dp2, dtype='datetime64[h]')
-
-    atide_pred = t_predic(
-        date_pred, names, freq, tidecon,
-        lat=lat0,ltype='nodal')
-
-
-    # compare astronomical tide data and prediction
-    atide_nanmean = xds_atide.tide - np.nanmean(xds_atide.tide)
-
-    # TODO: ADD PLOTS
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots()
-    ax.plot(xds_atide.time.values, atide_nanmean, 'k-', linewidth=0.5, label='data')
-    ax.plot(date_pred, atide_pred, 'r--', linewidth=0.5, label='model')
-    ax.set_xlim([np.datetime64('1998-06-01'), np.datetime64('2000-01-01')])
-    ax.legend()
-    plt.show()
-
-
+# Plot astronomical tide prediction
+time = xds_atide_sim.time.values[:]
+tide = xds_atide_sim.tide.values[:]
+p_export = op.join(p_export_tds, 'astronomical_tide_pred.png')
+Plot_AstronomicalTide(time, tide, p_export)
 
