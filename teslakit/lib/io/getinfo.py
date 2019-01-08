359c9eb10f547122f5275aa506c19d1b4512651c
Nicolás Ripoll Cabarga <nico@MacBook-Air-de-Nicolas.local>
2019-01-08 21:54:18 +0100

    added small function for getting some kind of files information

---
+++ teslakit/lib/io/getinfo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# basic import
+import os
+import os.path as op
+
+import netCDF4 as nc
+
+from lib.io.matlab import ReadMatfile
+
+
+def description(p):
+    'returns description of the file'
+    print 'getting info... {0}'.format(p)
+
+    txt = '\n\n\n-->{0}'.format(p)
+    if op.isdir(p):
+        txt += '\n*** FOLDER ***\n\nfiles:\n'
+        txt += ', '.join(os.listdir(p))
+        txt += '\n'
+
+    elif p.endswith('.nc'):
+        txt += '\n*** NETCDF File ***\n\n'
+        txt += str(nc.Dataset(p))
+
+    elif p.endswith('.mat'):
+        txt += '\n*** MATLAB File ***\n\nvariables:\n'
+        try:
+            txt += ', '.join(ReadMatfile(p).keys())
+        except:
+            txt +='\n couldn\'t read file.'
+        txt += '\n'
+
+    return txt
