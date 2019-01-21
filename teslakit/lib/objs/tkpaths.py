2e79ac2b5a747464e5e6c087193b61316a88a55e
Nicolás Ripoll Cabarga <nico@MacBook-Air-de-Nicolas.local>
2019-01-21 17:26:52 +0100

    site and database file paths now in .ini files

--- teslakit/lib/objs/tkpaths.py
+++ teslakit/lib/objs/tkpaths.py
@@ -70,11 +70,13 @@ class PathControl(object):
 
         # teslakit database and sites
         p_DB = op.join(p_data, 'database')
+        p_DB_ini = op.join(p_data, 'database.ini')
         p_sites = op.join(p_data, 'sites')
 
         # string paths
         self.p_data = p_data
         self.p_DB = p_DB
+        self.p_DB_ini = p_DB_ini
         self.p_sites = p_sites
         self.p_site = None  # current site
 
@@ -92,19 +94,21 @@ class PathControl(object):
         txt+= '\nDatabase Files:'
         for k1 in sorted(self.DB.keys()):
             for k2 in sorted(self.DB[k1].keys()):
-                aux ='\n.DB.{0}.{1}'.format(k1, k2)
-                #txt+='{0:27} - {1}'.format(aux, self.DB[k1][k2])
-                txt+='{0:.<45} {1}'.format(
-                    aux, self.DB[k1][k2].split('teslakit')[1])
+                aux1 ='\n.DB.{0}.{1}'.format(k1, k2)
+                aux2 = self.DB[k1][k2]
+                if 'teslakit' in aux2:
+                    aux2 = aux2.split('teslakit')[1]
+                txt+='{0:.<45} {1}'.format(aux1, aux2)
 
         if isinstance(self.site,dict):
             txt+= '\n\nSite Files:'
             for k1 in sorted(self.site.keys()):
                 for k2 in sorted(self.site[k1].keys()):
-                    aux ='\n.site.{0}.{1}'.format(k1, k2)
-                    #txt+='{0:27} - {1}'.format(aux, self.site[k1][k2])
-                    txt+='{0:.<45} {1}'.format(
-                        aux, self.site[k1][k2].split('teslakit')[1])
+                    aux1 ='\n.site.{0}.{1}'.format(k1, k2)
+                    aux2 = self.site[k1][k2]
+                    if 'teslakit' in aux2:
+                        aux2 = aux2.split('teslakit')[1]
+                    txt+='{0:.<45} {1}'.format(aux1, aux2)
         return txt
 
     def FilesSummary(self):
@@ -125,49 +129,24 @@ class PathControl(object):
         return txt
 
     def SetDatabase(self):
-        'Set paths not related with a study site'
+        'Set database paths from database.ini file'
 
-        # tropical cyclones 
-        pt = op.join(self.p_DB, 'TCs')
-        dd_tcs = {
-            'nakajo_mats': op.join(pt, 'Nakajo_tracks'),
-            'noaa':        op.join(pt, 'Allstorms.ibtracs_wmo.v03r10.nc'),
-            'noaa_fix':    op.join(pt, 'Allstorms.ibtracs_wmo.v03r10_fix.nc'),
-        }
 
-        # MJO
-        pt = op.join(self.p_DB, 'MJO')
-        dd_mjo = {
-            'hist': op.join(pt, 'MJO_hist.nc'),
-        }
+        p_DB = self.p_DB
+        p_ini = self.p_DB_ini
 
-        # SST
-        pt = op.join(self.p_DB, 'SST')
-        dd_sst = {
-            'hist_pacific' :op.join(pt, 'SST_1854_2017_Pacific.nc'),
-        }
+        # use configparser lib
+        cfg = configparser.ConfigParser()
+        cfg.read(p_ini)
 
-        # SLP
-        pt = op.join(self.p_DB, 'CFS')
-        dd_slp = {
-            'cfs_prmsl': op.join(pt, 'prmsl'),
-        }
+        dd = {}
+        for k in cfg.sections():
+            ss = atdict(cfg[k])
+            for kk in ss.keys():
+                if not ss[kk].startswith(os.sep) and not ':\\' in ss[kk]:
+                    ss[kk] = op.join(p_DB, k, ss[kk])
+            dd[k] = atdict(ss)
 
-        # RAW download
-        pt = op.join(self.p_DB, 'download')
-        dd_dwl = {
-            'CSIRO':op.join(pt, 'CSIRO'),
-        }
-
-        # main database dict
-        dd = {
-            'tcs': atdict(dd_tcs),
-            'mjo': atdict(dd_mjo),
-            'sst': atdict(dd_sst),
-            'slp': atdict(dd_slp),
-            'sst': atdict(dd_sst),
-            'dwl': atdict(dd_dwl),
-        }
         self.DB = atdict(dd)
 
     def SetSite(self, site_name):
@@ -175,95 +154,20 @@ class PathControl(object):
 
         # site folder 
         p_site = op.join(self.p_sites, site_name)
+        p_ini = op.join(p_site, 'files.ini')
 
-        # SST
-        pt = op.join(p_site, 'SST')
-        dd_sst = {
-            'PCA': op.join(pt, 'SST_PCA.nc'),
-            'KMA': op.join(pt, 'SST_KMA.nc'),
-            'alrw': op.join(pt, 'alr_w'),
-            'PCs_sim': op.join(pt, 'SST_PCs_sim.nc'),
-            'PCs_sim_d': op.join(pt, 'SST_PCs_sim_d.nc'),
-            'PCs_sim_m': op.join(pt, 'SST_PCs_sim_m.nc'),
+        # use configparser lib
+        cfg = configparser.ConfigParser()
+        cfg.read(p_ini)
 
-        }
+        dd = {}
+        for k in cfg.sections():
+            ss = atdict(cfg[k])
+            for kk in ss.keys():
+                if not ss[kk].startswith(os.sep) and not ':\\' in ss[kk]:
+                    ss[kk] = op.join(p_site, k, ss[kk])
+            dd[k] = atdict(ss)
 
-        # mjo
-        pt = op.join(p_site, 'MJO')
-        dd_mjo = {
-            'alrw': op.join(pt, 'alr_w'),       # auto regresive logistic sim 
-            'sim':  op.join(pt, 'MJO_sim.nc'),  # sim file
-        }
-
-        # estela predictor
-        pt = op.join(p_site, 'ESTELA')
-        dd_estela = {
-            'estelamat': op.join(pt, 'kwajalein_roi_obj.mat'),
-            'coastmat':  op.join(pt, 'Costa.mat'),
-            'gowpoint':  op.join(pt, 'gow2_062_ 9.50_167.25.mat'),
-            'slp':       op.join(pt, 'SLP.nc'),
-            'pred_slp':  op.join(pt, 'pred_SLP'),
-            'hydrographs':  op.join(pt, 'bins_hydrographs.pk'),  # intradaily
-            'alrw':      op.join(pt, 'alr_w'),       # auto regresive logistic sim 
-        }
-
-        # tropical cyclones
-        pt = op.join(p_site, 'TCs')
-        dd_tcs = {
-            'circle_hist': op.join(pt, 'TCs_hist_circle.nc'),
-            'probs_synth': op.join(pt, 'TCs_synth_ProbsChange.nc'),
-        }
-
-        # tide gauges 
-        pt = op.join(p_site, 'TIDE')
-        dd_tds = {
-            'mareografo':  op.join(pt, 'Mareografo_KWA.mat'),
-            'mareografo_nc':  op.join(pt, 'Mareografo_KWA.nc'),
-            'hist_astro': op.join(pt, 'tide_astro_hist.nc'),
-            'sim_astro': op.join(pt, 'tide_astro_sim.nc'),
-            'sim_mmsl': op.join(pt, 'tide_mmsl_sim.nc'),
-        }
-
-        # waves data 
-        p_wvs_parts = op.join(p_site, 'WAVES', 'wave_partitions')
-        p_wvs_procs = op.join(p_site, 'WAVES', 'wave_process')
-        dd_waves = {
-
-            # input
-            'partitions': p_wvs_parts,
-            'partitions_p1': op.join(p_wvs_parts, 'point1.mat'),
-
-            # execution files
-            'process': p_wvs_procs,
-            'partitions_noTCs': op.join(p_wvs_procs, 'waves_partitions_noTCs.nc'),
-            'families_noTCs':   op.join(p_wvs_procs, 'waves_families_noTCs.nc'),
-        }
-
-        # n-years simulation files 
-        #dd_sim = {
-        #    'mjo':op.join(p_site, 'SIM', 'MJO_sim.nc'),
-        #}
-
-        # export figs and reports
-        pt = op.join(p_site, 'export_figs')
-        dd_export = {
-            'mjo': op.join(pt, 'mjo'),
-            'sst': op.join(pt, 'sst'),
-            'tds': op.join(pt, 'tds'),
-        }
-
-
-        # main site dict
-        dd = {
-            'sst': atdict(dd_sst),
-            'mjo': atdict(dd_mjo),
-            'est': atdict(dd_estela),
-            'tcs': atdict(dd_tcs),
-            'tds': atdict(dd_tds),
-            'wvs': atdict(dd_waves),
-            #'sim': atdict(dd_sim),
-            'exp': atdict(dd_export),
-        }
         self.site = atdict(dd)
         self.p_site = p_site
         self.p_site_ini = op.join(p_site, 'site.ini')
