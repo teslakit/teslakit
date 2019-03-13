0054c0092994004960c41e8f0f149e1d56823c64
Nicolás Ripoll Cabarga <nico@MacBook-Air-de-Nicolas.local>
2019-03-13 18:59:24 +0100

    Moved Persistences function to lib/KMA.py

--- teslakit/lib/statistical.py
+++ teslakit/lib/statistical.py
@@ -10,31 +10,6 @@ from scipy.special import ndtri  # norm inv
 from scipy.stats import t  # t student
 import matplotlib.pyplot as plt
 
-def Persistences(series):
-    'Return series persistences for each element'
-
-    # output dict
-    d_pers = {}
-    for e in set(series):
-        d_pers[e] = []
-
-    # analize series
-    e0 = None
-    while series.any():
-
-        # pol left
-        e1 = series[0]
-        series = np.delete(series, 0)
-
-        if e1 != e0:
-            d_pers[e1].append(1)
-        else:
-            d_pers[e1][-1]+=1
-
-        # step forward
-        e0 = e1
-
-    return d_pers
 
 def ksdensity_CDF(x):
     '''
