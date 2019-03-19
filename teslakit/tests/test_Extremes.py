8f8150fb693cfc5e4ee1287f148beb09ce51eee4
Nicolás Ripoll Cabarga <nico@MacBook-Air-de-Nicolas.local>
2019-03-19 17:21:53 +0100

    test for extremes/GEV related functions updated

--- teslakit/tests/test_Extremes.py
+++ teslakit/tests/test_Extremes.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -15,8 +14,8 @@ import numpy as np
 # custom libs
 from lib.extremes import FitGEV_KMA_Frechet as FitGEV
 from lib.extremes import SampleGEV_KMA_Smooth as SampleGEV
-
 from lib.extremes import ChromosomesProbabilities_KMA as ChromProbs
+from lib.extremes import Correlation_Smooth_Partitions as CorrSP
 
 
 # --------------------------------------
@@ -24,49 +23,67 @@ from lib.extremes import ChromosomesProbabilities_KMA as ChromProbs
 p_data = op.join(op.dirname(__file__),'..','data')
 p_data = op.join(p_data, 'tests', 'test_ExtremesGEV')
 
-p_WT_wvs = op.join(p_data, 'xds_WT_wvs.nc')
-p_WT_KMA = op.join(p_data, 'xds_WT_KMA.nc')
 
-xds_WT_wvs = xr.open_dataset(p_WT_wvs)
-xds_WT_KMA = xr.open_dataset(p_WT_KMA)
+
+
+# --------------------------------------
+# Load test data
+xds_WT_wvs = xr.open_dataset(op.join(p_data, 'xds_WT_wvs_fam_noTCs.nc'))
+xds_WT_KMA = xr.open_dataset(op.join(p_data, 'xds_WT_KMA.nc'))
+
+# data to use
+n_clusters = 36
+bmus = xds_WT_KMA.bmus.values
+cenEOFs = xds_WT_KMA.cenEOFs.values
 
 
 # --------------------------------------
 # test chromosomes
-bmus = xds_WT_KMA.bmus.values
-n_clusters = 36
-var = xds_WT_wvs.swell_1_Hs.values
 
 # variables to get chrom probs
-se_Hs = xds_WT_wvs.sea_Hs.values
-s1_Hs = xds_WT_wvs.swell_1_Hs.values
-s2_Hs = xds_WT_wvs.swell_2_Hs.values
-np_cvs = np.column_stack([se_Hs, s1_Hs, s2_Hs])
+#se_Hs = xds_WT_wvs.sea_Hs.values
+#s1_Hs = xds_WT_wvs.swell_1_Hs.values
+#s2_Hs = xds_WT_wvs.swell_2_Hs.values
+#np_cvs = np.column_stack([se_Hs, s1_Hs, s2_Hs])
 
-chrom, chrom_probs = ChromProbs(bmus, n_clusters, np_cvs)
-print(chrom)
-print()
-print(chrom_probs)
-print()
+#chrom, chrom_probs = ChromProbs(bmus, n_clusters, np_cvs)
 
 
 # --------------------------------------
-# TODO Sigma, correlacion Tm,Hs suavizado partitions 
+# Sigma, correlacion Tm, Hs suavizado partitions 
+
+# load stored data
+#xds_gev_params = xr.open_dataset(op.join(p_data, 'test_xds_gev_paramsfit.nc'))
+#chrom = np.load(op.join(p_data, 'chrom.npy'))
+
+# TODO: repasar args y documentar 
+# get sigma
+#wvs_fams = ['sea', 'swell_1', 'swell_2']
+#sigma = CorrSP(
+#    bmus, cenEOFs, n_clusters, xds_WT_wvs, wvs_fams,
+#    xds_gev_params, chrom)
 
 
 # --------------------------------------
-# test GEV: Fit and Sample
-bmus = xds_WT_KMA.bmus.values
-n_clusters = 36
-var = xds_WT_wvs.swell_1_Hs.values
+# test GEV: Fit 
+
+# test one waves var
+#var = xds_WT_wvs.swell_1_Hs.values
 
 # Fit
-gev_params = FitGEV(bmus, n_clusters, var)
-print(gev_params)
-print()
+#gev_params = FitGEV(bmus, n_clusters, var)
+#print(gev_params)
+#print()
+
+
+# --------------------------------------
+# test GEV: Sample and climate emulator
+xds_gev_params = xr.open_dataset(op.join(p_data, 'test_xds_gev_paramsfit.nc'))
+chrom = np.load(op.join(p_data, 'chrom.npy'))
 
 # TODO: Sample GEV parameters for simulation
-paramS = SampleGEV(bmus, n_clusters, gev_params, var)
+#paramS = SampleGEV(bmus, n_clusters, gev_params, var)
 print(paramS)
 print()
+
 
