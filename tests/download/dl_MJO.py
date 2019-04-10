35bb3cb9fb47b1bbe3659fce0af68b5d8bae6aea
Nicolás Ripoll Cabarga <nico@MacBook-Air-de-Nicolas.local>
2019-04-10 13:31:20 +0200

    MJO download example updated

--- tests/download/dl_MJO.py
+++ tests/download/dl_MJO.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# basic import
+# common 
+import os
 import os.path as op
 import sys
-sys.path.insert(0, op.join(op.dirname(__file__),'..'))
+sys.path.insert(0, op.join(op.dirname(__file__),'..','..'))
 
 # tk libs
-from lib.objs.tkpaths import PathControl
-from lib.data_fetcher.MJO import Download_MJO
+from teslakit.project_site import Site
+from teslakit_downloader.MJO import Download_MJO
 
 
 # --------------------------------------
-# data storage and path control
-pc = PathControl()
+# test data storage
+
+site = Site('KWAJALEIN_TEST')
+DB = site.pc.DB                        # common database
+p_mjo_hist = DB.MJO.hist               # historical MJO
 
 # Download MJO and save to netcdf
 y1 = '1979-01-01'
-xds_mjo_hist = Download_MJO(pc.DB.mjo.hist, init_year=y1, log=True)
+xds_mjo_hist = Download_MJO(DB.MJO.hist, init_year=y1, log=True)
+print(xds_mjo_hist)
 
