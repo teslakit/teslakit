a0280d2a8ee74f54716f9b3a1dc12fad55c57c2a
Nicolás Ripoll Cabarga <nico@MacBook-Air-de-Nicolas.local>
2019-06-05 22:06:43 +0200

    tests climate emulator annual maxima added

---
+++ tests/ClimateEmulator/ml_annualmax.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# basic import
+import os
+import os.path as op
+import sys
+sys.path.insert(0, op.join(op.dirname(__file__),'..','..'))
+
+# python libs
+import numpy as np
+import xarray as xr
+
+# custom libs
+from teslakit.project_site import PathControl
+from teslakit.waves import TWL_WavesFamilies, TWL_AnnualMaxima
+from teslakit.io.matlab import ReadMatfile
+from teslakit.custom_dateutils import DateConverter_Mat2Py as dmp
+
+
+# --------------------------------------
+# Test data storage
+
+pc = PathControl()
+p_tests = pc.p_test_data
+p_test = op.join(p_tests, 'ClimateEmulator', 'ml_annualmaxima')
+
+
+# --------------------------------------
+# load test data (matlab ) 
+
+p_sim = op.join(p_test, 'test_sim.mat')
+dm_sim = ReadMatfile(p_sim)['dmat']
+sim_data = dm_sim['Sim']
+time_dnum = dm_sim['datenum']
+
+# mount compatible xarray.Dataset
+xds_wvs_sim = xr.Dataset(
+    {
+        'sea_Hs'      : (('time',), sim_data[:,0]),
+        'sea_Tp'      : (('time',), sim_data[:,1]),
+        'sea_Dir'     : (('time',), sim_data[:,2]),
+        'swell_1_Hs'  : (('time',), sim_data[:,3]),
+        'swell_1_Tp'  : (('time',), sim_data[:,4]),
+        'swell_1_Dir' : (('time',), sim_data[:,5]),
+        'swell_2_Hs'  : (('time',), sim_data[:,6]),
+        'swell_2_Tp'  : (('time',), sim_data[:,7]),
+        'swell_2_Dir' : (('time',), sim_data[:,8]),
+    },
+    coords = {'time' : dmp(time_dnum)}
+)
+
+
+
+# --------------------------------------
+# Calculate TWL for waves families data 
+
+xds_TWL = TWL_WavesFamilies(xds_wvs_sim)
+print(xds_TWL)
+print()
+
+
+# Calculate annual maxima (manually: time index not monotonic)
+xds_TWL_AnMax = TWL_AnnualMaxima(xds_TWL)
+print(xds_TWL_AnMax)
+
