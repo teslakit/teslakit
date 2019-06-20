fb5ee0f2388d1174b493c31d1eafd67a529e6d43
Nicolás Ripoll Cabarga <nico@MacBook-Air-de-Nicolas.local>
2019-06-20 02:31:35 +0200

    package init files updated for loading modules. teslakit module attrs updated

--- teslakit/__init__.py
+++ teslakit/__init__.py
@@ -1,7 +1,32 @@
-__version__='0.8.0'
-__author__='Surf And Surge Research Group (UC)'
-__contact__='ripolln@unican.es'
-__url__='https://bitbucket.org/ripolln/teslakit'
-__description__='Teslakit Waves Prediction libraries and methodology'
-__keywords__='wave prediction statistics analysys'
+"""
+Module attrs
+"""
+
+__version__     = '0.9.0'
+__author__      = 'Surf And Surge Research Group (UC)'
+__contact__     = 'ripolln@unican.es'
+__url__         = 'https://bitbucket.org/ripolln/teslakit'
+__description__ = 'Teslakit Waves Prediction libraries and methodology'
+__keywords__    = 'wave prediction statistics analysys'
+
+from . import alr
+from . import climate_emulator
+from . import custom_dateutils
+from . import estela
+from . import extremes
+from . import intradaily
+from . import kma
+from . import mda
+from . import mjo
+from . import pca
+from . import project_site
+from . import rbf
+from . import statistical
+from . import storms
+from . import tides
+from . import waves
+
+from . import io
+from . import plotting
+from . import util
 
---
+++ teslakit/plotting/__init__.py
@@ -0,0 +1,12 @@
+from . import alr
+from . import csiro
+from . import eofs
+#from . import estela  # requires cartopy
+from . import extremes
+from . import intradaily
+from . import kma
+from . import mjo
+from . import storms
+from . import tides
+from . import util
+
