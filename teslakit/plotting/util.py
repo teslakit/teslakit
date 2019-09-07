df074cc5019dfcdec2337681791d70865564dabb
Nicolás Ripoll Cabarga <nico@MacBook-Air-de-Nicolas.local>
2019-09-07 15:24:02 +0200

    renamed import

--- teslakit/plotting/util.py
+++ teslakit/plotting/util.py
@@ -3,10 +3,10 @@
 
 # pip
 import numpy as np
-import matplotlib.colors as colors
+import matplotlib.colors as mcolors
 
 
-class MidpointNormalize(colors.Normalize):
+class MidpointNormalize(mcolors.Normalize):
 	'''
 	Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
 	e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
@@ -14,7 +14,7 @@ class MidpointNormalize(colors.Normalize):
 
 	def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
 		self.midpoint = midpoint
-		colors.Normalize.__init__(self, vmin, vmax, clip)
+		mcolors.Normalize.__init__(self, vmin, vmax, clip)
 
 	def __call__(self, value, clip=None):
 		x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
