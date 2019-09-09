ccb7bdd51fe19baf5775819e6fe87456f31550e4
Nicolás Ripoll <ripollcab@gmail.com>
2019-09-09 19:07:19 +0200

    example renamed: ALR reports plot example

---
+++ scripts/examples/ALR/plot_ALRw_reports.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# common
+import os
+import os.path as op
+import sys
+
+# pip
+import numpy as np
+import xarray as xr
+
+# DEV: override installed teslakit
+import sys
+sys.path.insert(0, op.join(op.dirname(__file__), '..', '..', '..'))
+
+# teslakit
+from teslakit.database import Database
+from teslakit.alr import ALR_WRP
+
+
+# --------------------------------------
+# Teslakit database
+
+p_data = r'/Users/nico/Projects/TESLA-kit/TeslaKit/data'
+db = Database(p_data)
+
+# set site
+db.SetSite('TESTNEW')
+
+# alr test
+#p_alrw = db.paths.site.MJO.alrw
+#p_alrw = db.paths.site.SST.alrw
+p_alrw = db.paths.site.ESTELA.alrw
+
+# ALR wrap
+ALRW = ALR_WRP(p_alrw)
+
+# show model report 
+#ALRW.Report_Fit()
+
+# show simulation report
+ALRW.Report_Sim()
+
