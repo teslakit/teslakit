abbedcb28e6686911a70dc3f1058e0f54c9e4576
Nicolás Ripoll <ripollcab@gmail.com>
2019-09-13 03:46:52 +0200

    added submodules to plotting/__init__.py

--- teslakit/plotting/__init__.py
+++ teslakit/plotting/__init__.py
@@ -5,10 +5,12 @@ from . import eofs
 from . import extremes
 from . import intradaily
 from . import kma
+from . import awt
 from . import mjo
 from . import storms
 from . import tides
 from . import util
 
 from . import custom_colors
+from . import config
 
