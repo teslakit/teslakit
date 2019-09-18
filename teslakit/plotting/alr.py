25d86df7ce5e37f51416460c8b3a5bf9423f5dd9
Nicolás Ripoll <ripollcab@gmail.com>
2019-09-18 19:00:07 +0200

    teslakit/plotting/alr.py added WT probabilities and WT probability change plots

--- teslakit/plotting/alr.py
+++ teslakit/plotting/alr.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 # pip
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import matplotlib.dates as mdates
 import matplotlib.colors as mcolors
 from matplotlib.colorbar import ColorbarBase
@@ -16,8 +17,9 @@ from scipy.interpolate import interp1d
 
 # teslakit
 from .util import MidpointNormalize
-from .custom_colors import colors_mjo, colors_dwt, colors_interp
+from .custom_colors import colors_awt, colors_mjo, colors_dwt, colors_interp
 from ..custom_dateutils import xds2datetime as xds2dt
+from ..kma import ClusterProbabilities, ChangeProbabilities
 
 # import constants
 from .config import _faspect, _fsize, _fdpi
@@ -570,6 +572,157 @@ def Plot_Compare_Covariate(num_clusters,
         fig.savefig(p_export, dpi=_fdpi)
         plt.close()
 
-def Plot_Compare_Validation():
-    return
+def axplot_ChangeProbs(ax, change_probs, wt_colors,
+                       ttl = '', vmin = 0, vmax = 1,
+                       cmap = 'Blues', cbar_ttl = ''):
+    'axes plot cluster change probabilities'
+
+    num_clusters = change_probs.shape[0]
+
+    # clsuter transition plot
+    pc = ax.pcolor(
+        change_probs,
+        cmap=cmap, vmin=vmin, vmax=vmax,
+    )
+
+    # add colorbar
+    cbar = plt.colorbar(pc, ax=ax)
+    cbar.ax.tick_params(labelsize=8)
+    if vmin != 0 or vmax !=1:
+        cbar.set_ticks(np.linspace(vmin, vmax, 6))
+    cbar.ax.set_ylabel(cbar_ttl, rotation=270, labelpad=20)
+
+    # customize axes
+    ax.set_xticks(np.arange(num_clusters)+0.5)
+    ax.set_yticks(np.arange(num_clusters)+0.5)
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.set_title(ttl, {'fontsize':10, 'fontweight':'bold'})
+
+    # add custom color axis
+    ccmap = mcolors.ListedColormap([tuple(r) for r in wt_colors])
+
+    # custom color axis positions
+    ax_pos = ax.get_position()
+    cax_x_pos = [ax_pos.x0, ax_pos.y0-0.03, ax_pos.width, 0.02]
+    cax_y_pos = [ax_pos.x0-0.03/_faspect, ax_pos.y0, 0.02/_faspect, ax_pos.height]
+
+    # custom color axis X
+    cax_x = ax.figure.add_axes(cax_x_pos)
+    cbar_x = ColorbarBase(
+        cax_x, cmap = ccmap, orientation='horizontal',
+        norm = mcolors.Normalize(vmin=0, vmax=num_clusters),
+    )
+    cbar_x.set_ticks([])
+
+    # custom color axis X
+    cax_y= ax.figure.add_axes(cax_y_pos)
+    cbar_y = ColorbarBase(
+        cax_y, cmap = ccmap, orientation='vertical',
+        norm = mcolors.Normalize(vmin=0, vmax=num_clusters),
+    )
+    cbar_y.set_ticks([])
+
+def axplot_Transition(ax, probs_change_fit, probs_change_sim,
+                      ttl='', color='black'):
+    'axes plot for scatter historical - simulation transition matrix comparison'
+
+    # clsuter transition fit vs. sim scatter plot
+    pc = ax.scatter(
+        probs_change_fit, probs_change_sim,
+        color=color, s=5, zorder=2,
+    )
+
+    # get max value for axis lim
+    axlim = np.ceil(10*np.max(probs_change_sim[:]))/10
+
+    # customize axes
+    ax.plot(([0,0],[1,1]), linestyle='dashed', linewidth=0.5, color='grey',
+            zorder=1)
+    ax.set_ylabel('Simulated', {'fontsize':8})
+    ax.set_xlabel('Historical', {'fontsize':8})
+    ax.grid(True, which='major', axis='both', linestyle='--', color='grey')
+    ax.tick_params(axis='both', which='major', labelsize=7)
+    ax.set_xlim([0, axlim])
+    ax.set_ylim([0, axlim])
+    ax.set_title(ttl, {'fontsize':10, 'fontweight':'bold'})
+
+def axplot_ClusterProbs(ax, cluster_probs_fit, cluster_probs_sim, wt_colors,
+                       ttl=''):
+    'axes plot for scatter historical - simulation cluster probs comparison'
+
+    # clsuter transition plot
+    for cf, cs, wc in zip(cluster_probs_fit, cluster_probs_sim, wt_colors):
+        ax.plot(cf, cs, marker='.', markersize=8, color=wc, zorder=2)
+
+    # get max value for axis lim
+    axlim = np.ceil(10*np.max(cluster_probs_sim[:]))/10
+
+    # customize axes
+    ax.plot(([0,0],[1,1]), linestyle='dashed', linewidth=0.5, color='grey',
+            zorder=1)
+    ax.set_ylabel('Simulated', {'fontsize':8})
+    ax.set_xlabel('Historical', {'fontsize':8})
+    ax.grid(True, which='major', axis='both', linestyle='--', color='grey')
+    ax.tick_params(axis='both', which='major', labelsize=7)
+    ax.set_xlim([0, axlim])
+    ax.set_ylim([0, axlim])
+    ax.set_title(ttl, {'fontsize':10, 'fontweight':'bold'})
+
+def Plot_Validation(num_clusters, bmus_values_hist, bmus_values_sim,
+                    p_export=None):
+
+    # TODO: make compatible with bmus_values_sim size [n_xim x bmus]
+
+    # get cluster colors
+    wt_colors = GetClusterColors(num_clusters)
+    wt_set = np.arange(num_clusters) + 1
+
+    # cluster probabilities
+    probs_c_fit = ClusterProbabilities(bmus_values_hist, wt_set)
+    probs_c_sim = ClusterProbabilities(bmus_values_sim, wt_set)
+
+    # cluster change probabilities
+    _, chprobs_fit = ChangeProbabilities(bmus_values_hist, wt_set)
+    _, chprobs_rnd = ChangeProbabilities(bmus_values_sim, wt_set)
+    chprobs_dif = chprobs_rnd - chprobs_fit
+
+    # figure
+    fig = plt.figure(figsize=(_faspect*_fsize, _fsize))
+
+    # layout
+    gs = gridspec.GridSpec(2, 3, wspace=0.10, hspace=0.35)
+    ax_chpr_fit = plt.subplot(gs[0, 0])
+    ax_chpr_rnd = plt.subplot(gs[0, 1])
+    ax_chpr_dif = plt.subplot(gs[0, 2])
+    ax_scat_tra = plt.subplot(gs[1, 0])
+    ax_scat_pbs = plt.subplot(gs[1, 2])
+
+    # cluster change probs axes 
+    axplot_ChangeProbs(ax_chpr_fit, chprobs_fit, wt_colors,
+                       'Historical WT Transition Probabilities')
+    axplot_ChangeProbs(ax_chpr_rnd, chprobs_rnd, wt_colors,
+                       'Simulation WT Transition Probabilities')
+    axplot_ChangeProbs(ax_chpr_dif, chprobs_dif, wt_colors,
+                       'Change in WT Transition Probabilities',
+                       vmin=-0.05, vmax=0.05, cmap='RdBu_r',
+                       cbar_ttl='Simulation - Historical',
+                      )
+
+    # scatter plot transitions
+    axplot_Transition(ax_scat_tra, chprobs_fit, chprobs_rnd,
+                      'Simualted vs. Historical WT Transition Probabilities'
+                     )
+
+    # scatter plot cluster probs
+    axplot_ClusterProbs(ax_scat_pbs, probs_c_fit, probs_c_sim, wt_colors,
+                       'Simulated vs. Historical WT Probabilities',
+                       )
+
+    # show / export
+    if not p_export:
+        plt.show()
+    else:
+        fig.savefig(p_export, dpi=_fdpi)
+        plt.close()
 
