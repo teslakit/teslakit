b9cf9857b074415ab6bd2cac1b497aaab8a5b6c6
Nicolás Ripoll <ripollcab@gmail.com>
2019-09-18 18:56:52 +0200

    teslakit/kma.py added functions for calculating bmus probability and probability change

--- teslakit/kma.py
+++ teslakit/kma.py
@@ -39,6 +39,41 @@ def Persistences(series):
 
     return d_pers
 
+def ClusterProbabilities(series, set_values):
+    'return series probabilities for each item at set_values'
+
+    us, cs = np.unique(series, return_counts=True)
+    d_count = dict(zip(us,cs))
+
+    # cluster probabilities
+    cprobs = np.zeros((len(set_values)))
+    for i, c in enumerate(set_values):
+       cprobs[i] = 1.0*d_count[c]/len(series) if c in d_count.keys() else 0.0
+
+    return cprobs
+
+def ChangeProbabilities(series, set_values):
+    'return series transition count and probabilities'
+
+    # count matrix
+    count = np.zeros((len(set_values), len(set_values)))
+    for ix, c1 in enumerate(set_values):
+        for iy, c2 in enumerate(set_values):
+
+            # count cluster-next_cluster ocurrences
+            us, cs = np.unique((series[:-1]==c1) & (series[1:]==c2), return_counts=True)
+            d_count = dict(zip(us,cs))
+            count[ix, iy] = d_count[True] if True in d_count.keys() else 0
+
+    # probabilities
+    probs = np.zeros((len(set_values), len(set_values)))
+    for ix, _ in enumerate(set_values):
+
+        # calculate each row probability
+        probs[ix,:] = count[ix,:] / np.sum(count[ix, :])
+
+    return count, probs
+
 def sort_cluster_gen_corr_end(centers, dimdim):
     '''
     SOMs alternative
