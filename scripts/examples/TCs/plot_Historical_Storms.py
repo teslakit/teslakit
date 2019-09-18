1bc3be6183a0ab6cba1a6e6492fb10edfce3e6e6
Nicolás Ripoll <ripollcab@gmail.com>
2019-09-18 21:21:29 +0200

    added world map storm plot examples

--- scripts/examples/TCs/plot_Historical_Storms.py
+++ scripts/examples/TCs/plot_Historical_Storms.py
@@ -16,7 +16,8 @@ sys.path.insert(0, op.join(op.dirname(__file__), '..', '..', '..'))
 
 # teslakit
 from teslakit.database import Database
-from teslakit.plotting.storms import Plot_TCs_TracksParams
+from teslakit.plotting.storms import Plot_TCs_TracksParams, \
+Plot_Historical_TCs_Tracks, Plot_Historical_TCs_Tracks_Category
 
 
 # --------------------------------------
@@ -30,8 +31,32 @@ db.SetSite('KWAJALEIN')
 
 
 # Load storms extracted at 4 degree radius 
-xds_tracks, xds_params = db.Load_TCs_r2()
+xds_tracks_r1, xds_params_r1 = db.Load_TCs_r1()
+xds_tracks_r2, xds_params_r2 = db.Load_TCs_r2()
+
 
 # Plot storms tracks and storm parametrized inside radius 
-Plot_TCs_TracksParams(xds_tracks, xds_params)
+Plot_TCs_TracksParams(xds_tracks_r2, xds_params_r2)
+
+
+# Plot storm tracks world map (requires basemap)
+lon1, lon2 = 90, 270
+lat1, lat2 = -20, 70
+pnt_lon, pnt_lat = 167.5, 9.75
+r1, r2 = 14, 4
+
+Plot_Historical_TCs_Tracks(
+    xds_tracks_r1, xds_tracks_r2,
+    lon1, lon2, lat1, lat2,
+    pnt_lon, pnt_lat, r1, r2,
+)
+
+# Plot storm tracks category world map (requires basemap)
+category = xds_params_r1.category
+
+Plot_Historical_TCs_Tracks_Category(
+    xds_tracks_r1, category,
+    lon1, lon2, lat1, lat2,
+    pnt_lon, pnt_lat, r1,
+)
 
