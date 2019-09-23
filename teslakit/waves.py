a0eae01ac97933593e8f93115854da1d487b406a
Laura Cagigal <lcag075@aucklanduni.ac.nz>
2019-09-23 21:23:46 +0200

    Added wave distribution plots

--- teslakit/waves.py
+++ teslakit/waves.py
@@ -132,6 +132,114 @@ def GetDistribution(xds_wps, swell_sectors):
 
     return xds_parts
 
+def GetDistribution_ws(xds_wps, swell_sectors):
+    '''
+    Separates wave partitions (0-5) into families.
+    Default: sea, swl1, swl2
+
+    xds_wps (waves partitionss):
+        xarray.Dataset (time,), phs, pspr, pwfrac... {0-5 partitions}
+
+    sectors: list of degrees to cut wave energy [(a1, a2), (a2, a3), (a3, a1)]
+
+    returns
+        xarray.Dataset (time,), fam_V, {fam: sea,swell_1,swell2. V: Hs,Tp,Dir}
+    '''
+
+    sea_Hs= xds_wps.isel(part=0).hs.values
+    sea_Tp=xds_wps.isel(part=0).tp.values
+    sea_Dir = xds_wps.isel(part=0).dpm.values
+    time= xds_wps.time.values
+
+    # concatenate energy groups 
+    cat_hs = np.column_stack(
+        (xds_wps.isel(part=1).hs.values,
+        xds_wps.isel(part=2).hs.values,
+        xds_wps.isel(part=3).hs.values,
+        xds_wps.isel(part=4).hs.values,
+        xds_wps.isel(part=5).hs.values)
+    )
+    cat_tp = np.column_stack(
+        (xds_wps.isel(part=1).tp.values,
+        xds_wps.isel(part=2).tp.values,
+        xds_wps.isel(part=3).tp.values,
+        xds_wps.isel(part=4).tp.values,
+        xds_wps.isel(part=5).tp.values)
+    )
+    cat_dir = np.column_stack(
+        (xds_wps.isel(part=1).dpm.values,
+        xds_wps.isel(part=2).dpm.values,
+        xds_wps.isel(part=3).dpm.values,
+        xds_wps.isel(part=4).dpm.values,
+        xds_wps.isel(part=5).dpm.values)
+    )
+
+    # prepare output array
+    xds_parts = xr.Dataset({
+        'sea_Hs':('time',sea_Hs),
+        'sea_Tp':('time',sea_Tp),
+        'sea_Dir':('time',sea_Dir)
+    },
+        coords = {'time':time}
+    )
+
+    # solve sectors
+    c = 1
+    for s_ini, s_end in swell_sectors:
+        if s_ini < s_end:
+            p_sw = np.where((cat_dir <= s_end) & (cat_dir > s_ini))
+        else:
+            p_sw = np.where((cat_dir <= s_end) | (cat_dir > s_ini))
+
+        # get data inside sector
+        sect_dir = np.zeros(cat_dir.shape)*np.nan
+        sect_hs = np.zeros(cat_dir.shape)*np.nan
+        sect_tp = np.zeros(cat_dir.shape)*np.nan
+
+        sect_dir[p_sw] = cat_dir[p_sw]
+        sect_hs[p_sw] = cat_hs[p_sw]
+        sect_tp[p_sw] = cat_tp[p_sw]
+
+        # calculate swell Hs, Tp, Dir
+        swell_Hs = np.sqrt(np.nansum(np.power(sect_hs,2), axis=1))
+
+        swell_Tp = np.sqrt(
+            np.nansum(np.power(sect_hs,2), axis=1) /
+            np.nansum(np.power(sect_hs,2)/np.power(sect_tp,2), axis=1)
+        )
+        swell_Dir = np.arctan2(
+            np.nansum(np.power(sect_hs,2) * sect_tp * np.sin(sect_dir*np.pi/180), axis=1),
+            np.nansum(np.power(sect_hs,2) * sect_tp * np.cos(sect_dir*np.pi/180), axis=1)
+        )
+
+        # dir correction and denormalization 
+        swell_Dir[np.where((swell_Dir<0))] = swell_Dir[np.where((swell_Dir<0))]+2*np.pi
+        swell_Dir = swell_Dir*180/np.pi
+
+        # dont do arctan2 if there is only one dir
+        i_onedir = np.where(
+            (np.count_nonzero(~np.isnan(sect_dir),axis=1)==1)
+        )[0]
+        swell_Dir[i_onedir] = np.nanmin(sect_dir[i_onedir], axis=1)
+
+        # out of bound dir correction
+        swell_Dir[np.where((swell_Dir>360))] = swell_Dir[np.where((swell_Dir>360))]-360
+        swell_Dir[np.where((swell_Dir<0))] = swell_Dir[np.where((swell_Dir<0))]+360
+
+
+        # fix swell all-nans to 0s nansum behaviour
+        p_fix = np.where(swell_Hs==0)
+        swell_Hs[p_fix] = np.nan
+        swell_Dir[p_fix] = np.nan
+
+        # append data to partitons dataset
+        xds_parts['swell_{0}_Hs'.format(c)] = ('time', swell_Hs)
+        xds_parts['swell_{0}_Tp'.format(c)] = ('time', swell_Tp)
+        xds_parts['swell_{0}_Dir'.format(c)] = ('time', swell_Dir)
+        c+=1
+
+    return xds_parts
+
 def TWL(hs, tp):
     'Returns Total Water Level'
 
