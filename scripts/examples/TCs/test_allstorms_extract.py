d0f1b6d181d4133f30bba05dd703d44e21d2c1c1
Nicolás Ripoll <ripollcab@gmail.com>
2019-10-11 20:07:27 +0200

    TCs extraction test updated

--- scripts/examples/TCs/test_allstorms_extract.py
+++ scripts/examples/TCs/test_allstorms_extract.py
@@ -40,6 +40,7 @@ pnt_lat = 9.75
 
 # radius for TCs selection (º)
 r1 = 14
+r2 = 4
 
 # TODO: CON RADIO 14, la 251 o 253 tiene valores mal de presion
 
@@ -47,14 +48,36 @@ r1 = 14
 # Select Historical TCs inside circle
 
 # dictionary with needed variable names
-d_vns = {
-    'longitude': 'lon_wmo',
-    'latitude': 'lat_wmo',
-    'time': 'time_wmo',
-    'pressure': 'pres_wmo',
-}
+#d_vns = {
+#    'longitude': 'lon_wmo',
+#    'latitude': 'lat_wmo',
+#    'time': 'time_wmo',
+#    'pressure': 'pres_wmo',
+#}
 
-# Select TCs that crosses a circular area R1
-xds_TCs_r1_tracks, xds_TCs_r1_params = Extract_Circle(
-    xds_wmo, pnt_lon, pnt_lat, r1, d_vns)
+## Select TCs that crosses a circular area R1
+#xds_TCs_r1_tracks, xds_TCs_r1_params = Extract_Circle(
+#    xds_wmo, pnt_lon, pnt_lat, r1, d_vns)
+
+## Select TCs that crosses a circular area R2
+#xds_TCs_r2_tracks, xds_TCs_r2_params = Extract_Circle(
+#    xds_wmo, pnt_lon, pnt_lat, r2, d_vns)
+
+# --------------------------------------
+# storms that enters r1 but not r2
+_, xds_TCs_r2_params = db.Load_TCs_r2()
+_, xds_TCs_r1_params = db.Load_TCs_r1()
+
+storms_r1 = xds_TCs_r1_params.storm.values[:]
+storms_r2 = xds_TCs_r2_params.storm.values[:]
+
+print(storms_r1, len(storms_r1))
+print()
+print(storms_r2, len(storms_r2))
+
+# storms that cross r1 but not r2
+storms_sel = [s for s in storms_r1 if not s in storms_r2]
+print()
+print(storms_sel, len(storms_sel))
+
 
