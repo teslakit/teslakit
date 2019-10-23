0376c9795bb495bbc33df61f7274c239aedcc35e
Nicolás Ripoll <ripollcab@gmail.com>
2019-10-23 14:00:42 +0200

    estela plot example updated

--- scripts/examples/ESTELA/plots_estela_predictor.py
+++ scripts/examples/ESTELA/plots_estela_predictor.py
@@ -32,12 +32,11 @@ db.SetSite('KWAJALEIN')
 # estela predictor
 xds_est = db.Load_ESTELA_data()
 
-
 # plot ESTELA (basemap)
 Plot_ESTELA(
     xds_est.pnt_longitude, xds_est.pnt_latitude,
-    xds_est.F_y1993to2012 * xds_est.mask_shadow,
-    xds_est.D_y1993to2012 * xds_est.mask_shadow,
+    xds_est.F_y1993to2012 * xds_est.mask_e95,
+    xds_est.D_y1993to2012 * xds_est.mask_e95,
     lon1=110, lon2=290, lat1=-50, lat2=70,
 )
 
@@ -53,7 +52,10 @@ pred.Plot_EOFs_EstelaPred()
 pred.Plot_DWT_PCs(n=6)
 
 # test DWTs mean plot
-pred.Plot_DWTs('SLP')
+pred.Plot_DWTs('SLP', kind='mean', show=True)
+
+# test DWTs anomally plot
+pred.Plot_DWTs('SLP', kind='anom', show=True)
 
 # test DWTs probs plot
 pred.Plot_DWTs_Probs()
