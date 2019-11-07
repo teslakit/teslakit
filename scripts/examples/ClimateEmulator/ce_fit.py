ddfe2962853fa6adf94f58b03b2567ea8ee0e337
Nicolás Ripoll <ripollcab@gmail.com>
2019-11-08 00:24:45 +0100

    updated ce_fit example script

--- scripts/examples/ClimateEmulator/ce_fit.py
+++ scripts/examples/ClimateEmulator/ce_fit.py
@@ -11,19 +11,17 @@ import xarray as xr
 
 # DEV: override installed teslakit
 import sys
-sys.path.insert(0,'../../../')
+sys.path.insert(0, op.join(op.dirname(__file__), '..', '..', '..'))
 
 # teslakit
-from teslakit.project_site import PathControl
 from teslakit.climate_emulator import Climate_Emulator
 
 
 # --------------------------------------
 # Test data storage
 
-pc = PathControl()
-p_tests = pc.p_test_data
-p_test = op.join(p_tests, 'ClimateEmulator', 'CE_FitExtremes')
+p_data = r'/Users/nico/Projects/TESLA-kit/TeslaKit/data'
+p_test = op.join(p_data, 'tests', 'ClimateEmulator', 'CE_FitExtremes')
 
 # input
 p_KMA = op.join(p_test, 'xds_KMA.nc')
@@ -40,6 +38,10 @@ p_ce = op.join(p_test, 'ce')  # climate emulator (fit)
 xds_KMA = xr.open_dataset(p_KMA)
 xds_WVS_fam = xr.open_dataset(p_WVS_fam)
 xds_WVS_pts = xr.open_dataset(p_WVS_pts)
+
+print(xds_KMA)
+print(xds_WVS_fam)
+print(xds_WVS_pts)
 
 
 # --------------------------------------
