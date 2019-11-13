86e1e208f8a930fb3a85ca94df105aabed066bab
Nicolás Ripoll <ripollcab@gmail.com>
2019-11-13 20:58:44 +0100

    added new example script for loading all simulated hourly data

---
+++ scripts/examples/database/load_simulation_hourly.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# common
+import os
+import os.path as op
+import sys
+
+# pip
+import numpy as np
+import xarray as xr
+
+# DEV: override installed teslakit
+import sys
+sys.path.insert(0, op.join(op.dirname(__file__), '..', '..', '..'))
+
+# teslakit
+from teslakit.database import Database
+from teslakit.plotting.wts import Plot_Probs_WT_WT
+
+
+
+# --------------------------------------
+# Teslakit database
+
+p_data = r'/Users/nico/Projects/TESLA-kit/TeslaKit/data'
+db = Database(p_data)
+
+# set site
+db.SetSite('KWAJALEIN')
+
+# load simulation data(hourly)
+data = db.Load_SIM_hourly()
+
+print(data)
+
