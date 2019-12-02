d654dd74a41e0fcf63801178fc0e3488211697fa
Nicolás Ripoll <ripollcab@gmail.com>
2019-12-02 12:47:01 +0100

    extremes.py Peaks Over Threshold method added

--- teslakit/extremes.py
+++ teslakit/extremes.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # pip
+from datetime import timedelta
 import numpy as np
+import xarray as xr
 from scipy.stats import  gumbel_l, genextreme
+
+from .util.time_operations import date2datenum as d2d
 
 def FitGEV_KMA_Frechet(bmus, n_clusters, var):
     '''
@@ -151,5 +155,152 @@ def ACOV(f, theta, x):
     acov = np.linalg.inv(FI)
 
     return acov
+
+def Peaks_Over_Threshold(xds, var_name, percentile=99, threshold=None,
+                         window_days=3):
+    '''
+
+    Peaks Over Threshold methodology to find extreme values.
+
+    xds         - xarray.Dataset (time dim)
+    var_name    - variable to apply POT
+    percentile  - if threshold not given, calculate it with this percentile
+    threshold   - optional, threshold to apply POT
+    window_days - minimum number of days between consecutive independent peaks
+
+    returns xarray.Dataset ('time', ) vars: peaks, excedeence, area, and duration
+    '''
+
+    # TODO: refactor with times
+    def to_hours(time_diff):
+
+        # single
+        if isinstance(time_diff, (timedelta, np.timedelta64)):
+            if isinstance(time_diff, np.timedelta64):
+                dt_h = time_diff / np.timedelta64(1, 'h')
+
+            elif isinstance(time_diff, timedelta):
+                dt_h = np.array(time_diff.total_seconds()/3600.0)
+
+            return dt_h
+
+        # else: array
+        if isinstance(time_diff[0], np.timedelta64):
+            dt_h = time_diff / np.timedelta64(1, 'h')
+
+        elif isinstance(time_diff[0], timedelta):
+            dt_h = np.array([x.total_seconds()/3600.0 for x in time_diff])
+
+        return dt_h
+
+
+    # get variable
+    vv = xds[var_name].values[:]
+    vt = xds['time'].values[:]
+
+    # data delta time (hours)
+    npdf = np.diff(vt)
+    dt_h = to_hours(npdf)
+
+    # threshold
+    if threshold == None:
+        threshold = np.percentile(vv, percentile)
+
+    # consecutive peaks over threshold
+    ind_mask = np.where(vv >= threshold, 1, 0)
+    ind_dif = np.diff(ind_mask)
+
+    # peaks start and end
+    ind_ini = np.where(ind_dif == 1)[0] + 1
+    ind_end = np.where(ind_dif == -1)[0] + 1
+
+    # start and end corrections
+    if ind_end[0]<ind_ini[0]: ind_ini = np.insert(ind_ini,0,0)
+    if ind_end[-1]<ind_ini[-1]: ind_end = np.append(ind_end, len(ind_dif))
+
+    # store variable max, time max, duration and area above threshold for each peak
+    time_max, vv_max, area, durac = [], [], [], []
+
+    for i, f in zip(ind_ini, ind_end):
+
+        vv_max.append(np.max(vv[i:f]))
+
+        area.append(np.sum(vv[i:f] * dt_h[i:f] - threshold))
+
+        ind_max = np.argmax(vv[i:f])
+        ind_time = list(range(i, f))
+        time_max.append(vt[ind_time[ind_max]])
+
+        durac.append(to_hours(vt[f] - vt[i]))
+
+
+    # ensure independence between maxs
+    ind_window_mask = np.where(to_hours(np.diff(time_max)) < 24*window_days, 1, 0)
+    if np.any(ind_window_mask):
+
+        # solve dependent events
+        ind_dif = np.diff(ind_window_mask)
+
+        # first event corrections 
+        ind_dif = np.insert(ind_dif, 0, 0)
+        if ind_dif[1] == -1: ind_dif[0] = 1
+
+        ind_ini = np.where(ind_dif == 1)[0]
+        ind_fin = np.where(ind_dif == -1)[0] + 1
+
+        # Keep maximum of dependent events
+        time_max_indep = []
+        vv_max_indep = []
+        durac_indep = []
+        area_indep = []
+        ind_delete = []
+        for ind_i, ind_f in zip(ind_ini, ind_fin):
+
+            vv_temp = np.max(vv_max[ind_i:ind_f])
+            vv_max_indep.append(vv_temp)
+
+            vv_ind_max = np.argmax(vv_max[ind_i:ind_f])
+            ind_time = list(range(ind_i,ind_f))
+            time_temp = time_max[ind_time[vv_ind_max]]
+            time_max_indep.append(time_temp)
+
+            durac_indep.append(
+                to_hours(time_max[ind_f]-time_max[ind_i])
+            )
+
+            area_indep.append(np.sum(area[ind_i:ind_f]))
+
+            ind_delete.extend(ind_time)
+
+        # remove all dependent events
+        vv_max = np.delete(vv_max, [ind_delete])
+        time_max = np.delete(time_max, [ind_delete])
+        durac = np.delete(durac, [ind_delete])
+        area = np.delete(area, [ind_delete])
+
+        # add independent events
+        vv_max = np.concatenate((vv_max, vv_max_indep))
+        time_max = np.concatenate((time_max, time_max_indep))
+        durac = np.concatenate((durac, durac_indep))  # hours
+        area = np.concatenate((area, area_indep))
+
+    # output
+    peaks = xr.Dataset(
+        {
+            '{0}'.format(var_name): (('time',), vv_max),
+            '{0}_exceedances'.format(var_name): (('time',), vv_max-threshold),
+            'duration': (('time',), durac),
+            'area': (('time',), area),
+        },
+        coords = {'time': time_max},
+    )
+    peaks = peaks.sortby('time')
+
+    # add some attrs
+    peaks.attrs['threshold'] = threshold
+    peaks.attrs['percentile'] = percentile
+
+    return peaks
+
 
 
