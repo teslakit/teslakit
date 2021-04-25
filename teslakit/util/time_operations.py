b8228b31f039b47983291c3d7230c366915aa0e2
Nicolás Ripoll <ripollcab@gmail.com>
2021-04-25 11:41:34 +0200

    bug patched: datetime.utcfromtimestamp was not working at windows O.S. if date to comvert is bellow 1970-01-01

--- teslakit/util/time_operations.py
+++ teslakit/util/time_operations.py
@@ -52,7 +52,7 @@ def npdt64todatetime(dt64):
     'converts np.datetime64[ns] into datetime'
 
     ts = (dt64 - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
-    return datetime.utcfromtimestamp(ts)
+    return datetime(1970, 1, 1) + timedelta(seconds=ts)
 
 # declare aux function for generating datetime.datetime array
 def generate_datetimes(t0, t1, dtype='datetime64[h]'):
